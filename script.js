b590e0e25ce800a9e52a427b33a285006f473711
pty.fer <103433870+Fer131007@users.noreply.github.com>
2024-05-28 08:49:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado= document.querySelector(".caixa-resultado");
+const textoResultado= document.querySelector(".texto-resultado");
 
+const perguntas = [
+  {
+      enunciado:"você tem um sono bom?"
+    alternativas:[
+    "sim";
+     "não";
+]
+},
+  {
+      enunciado:"você tem uma alimentação boa"
+    alternativas:[
+    "sim";
+     "não";
+]
+},
+  {
+      enunciado:"você pratica esportes?"
+    alternativas:[
+    "sim";
+     "não";
+]
+},
+  {
+      enunciado:""
+    alternativas:[
+    "alternativa 1";
+     "alternativa 2";
+]
+},
+  {
+      enunciado:"perguntas 5"
+    alternativas:[
+    "alternativa 1";
+     "alternativa 2";
+]
+},
+];
